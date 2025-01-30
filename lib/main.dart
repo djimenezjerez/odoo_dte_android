a17b27bc1f27a38de8266ba76061be1ae77c8d5e
djimenezjerez <djimenezjerez@gmail.com>
2025-01-30 09:21:06 -0400

    Agregado stack para mostrar ventana de carga

--- lib/main.dart
+++ lib/main.dart
@@ -41,116 +41,117 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             toolbarHeight: 0,
           ),
-          body: Container(
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: InAppWebView(
-                    initialUrlRequest: URLRequest(
-                      url: WebUri('http://190.186.18.34:805'),
-                    ),
-                    initialSettings: InAppWebViewSettings(
-                      javaScriptEnabled: true,
-                      domStorageEnabled: true,
-                      supportMultipleWindows: true,
-                      useOnDownloadStart: true,
-                    ),
-                    onPermissionRequest: (controller, request) async {
-                      return PermissionResponse(
-                          resources: request.resources,
-                          action: PermissionResponseAction.GRANT);
-                    },
-                    onWebViewCreated: (controller) {
-                      this.webViewController = controller;
+          body: Stack(
+            children: [
+              Column(
+                children: <Widget>[
+                  Expanded(
+                    child: InAppWebView(
+                      initialUrlRequest: URLRequest(
+                        url: WebUri('http://190.186.18.34:805'),
+                      ),
+                      initialSettings: InAppWebViewSettings(
+                        javaScriptEnabled: true,
+                        domStorageEnabled: true,
+                        supportMultipleWindows: true,
+                        useOnDownloadStart: true,
+                      ),
+                      onPermissionRequest: (controller, request) async {
+                        return PermissionResponse(
+                            resources: request.resources,
+                            action: PermissionResponseAction.GRANT);
+                      },
+                      onWebViewCreated: (controller) {
+                        this.webViewController = controller;
 
-                      // Agregar el handler para recibir el archivo desde JavaScript
-                      webViewController.addJavaScriptHandler(
-                        handlerName: "downloadBlob",
-                        callback: (args) async {
-                          try {
-                            setState(() {
-                              isDownloading = true;
-                            });
+                        // Agregar el handler para recibir el archivo desde JavaScript
+                        webViewController.addJavaScriptHandler(
+                          handlerName: "downloadBlob",
+                          callback: (args) async {
+                            try {
+                              setState(() {
+                                isDownloading = true;
+                              });
 
-                            String base64Data = args[0].toString().split(',')[1];
+                              String base64Data = args[0].toString().split(',')[1];
 
-                            // Obtener la fecha y hora actual
-                            DateTime fecha_actual = DateTime.now();
-                            String nombre_archivo = fecha_actual.toUtc().millisecondsSinceEpoch.toString();
+                              // Obtener la fecha y hora actual
+                              DateTime fecha_actual = DateTime.now();
+                              String nombre_archivo = fecha_actual.toUtc().millisecondsSinceEpoch.toString();
 
-                            // Obtener la carpeta de Descargas
-                            Directory? downloadsDirectory = await getDownloadsDirectory();
-                            if (downloadsDirectory == null) {
-                              downloadsDirectory = await getApplicationDocumentsDirectory();
+                              // Obtener la carpeta de Descargas
+                              Directory? downloadsDirectory = await getDownloadsDirectory();
+                              if (downloadsDirectory == null) {
+                                downloadsDirectory = await getApplicationDocumentsDirectory();
+                              }
+
+                              String filePath = '${downloadsDirectory?.path}/$nombre_archivo.pdf';
+
+                              File file = File(filePath);
+                              await file.writeAsBytes(base64Decode(base64Data));
+
+                              Fluttertoast.showToast(
+                                msg: 'Archivo guardado en: $filePath',
+                                backgroundColor: Colors.green,
+                                textColor: Colors.white,
+                                fontSize: 16.0,
+                              );
+                            } catch (e) {
+                              Fluttertoast.showToast(
+                                msg: 'Error al descargar: $e',
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0,
+                              );
+                            } finally {
+                              setState(() {
+                                isDownloading = false;
+                              });
                             }
-
-                            String filePath = '${downloadsDirectory?.path}/$nombre_archivo.pdf';
-
-                            File file = File(filePath);
-                            await file.writeAsBytes(base64Decode(base64Data));
-
-                            Fluttertoast.showToast(
-                              msg: 'Archivo guardado en: $filePath',
-                              backgroundColor: Colors.green,
-                              textColor: Colors.white,
-                              fontSize: 16.0,
-                            );
-                          } catch (e) {
-                            Fluttertoast.showToast(
-                              msg: 'Error al descargar: $e',
-                              backgroundColor: Colors.red,
-                              textColor: Colors.white,
-                              fontSize: 16.0,
-                            );
-                          } finally {
-                            setState(() {
-                              isDownloading = false;
-                            });
-                          }
-                        },
-                      );
-                    },
-                    // ESTE ES EL MANEJO DE VENTANA NUEVA
-                    onCreateWindow: (controller, createWindowRequest) async {
-                      await Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) {
-                          return Scaffold(
-                            appBar: AppBar(
-                              title: Text('Nueva pestaña'),
-                            ),
-                            body: InAppWebView(
-                              initialUrlRequest: createWindowRequest.request,
-                              initialSettings: InAppWebViewSettings(
-                                javaScriptEnabled: true,
-                                domStorageEnabled: true,
-                                supportMultipleWindows: true,
-                              ),
-                              onDownloadStartRequest: (controller, url) async {
-                              },
-                            ),
-                          );
-                        }),
-                      );
-                      return true;
-                    },
-                    onDownloadStartRequest: (controller, url) async {
-                      setState(() {
-                        isDownloading = true; // 🔹 Activa el indicador de carga
-                      });
-
-                      String fileUrl = url.url.uriValue.toString();
-                      debugPrint('**** ----- !!!! URL del archivo: $fileUrl , MimeType: ${url.mimeType}');
-                      if (fileUrl.toLowerCase().startsWith('blob:') || url.mimeType == 'application/pdf' || fileUrl.toLowerCase().startsWith("data:application/octet-stream;base64,")){
-                        Fluttertoast.showToast(
-                          msg: 'BLOB, intentando descargar... \n ${url.mimeType} \n $fileUrl',
-                          backgroundColor: Colors.orange,
-                          textColor: Colors.white,
-                          fontSize: 16.0,
+                          },
                         );
+                      },
+                      // ESTE ES EL MANEJO DE VENTANA NUEVA
+                      onCreateWindow: (controller, createWindowRequest) async {
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) {
+                            return Scaffold(
+                              appBar: AppBar(
+                                title: Text('Nueva pestaña'),
+                              ),
+                              body: InAppWebView(
+                                initialUrlRequest: createWindowRequest.request,
+                                initialSettings: InAppWebViewSettings(
+                                  javaScriptEnabled: true,
+                                  domStorageEnabled: true,
+                                  supportMultipleWindows: true,
+                                ),
+                                onDownloadStartRequest: (controller, url) async {
+                                },
+                              ),
+                            );
+                          }),
+                        );
+                        return true;
+                      },
+                      onDownloadStartRequest: (controller, url) async {
+                        setState(() {
+                          isDownloading = true; // 🔹 Activa el indicador de carga
+                        });
 
-                        // Ejecuta JavaScript para convertir el Blob en un archivo descargable
-                        String jsCode = """
+                        String fileUrl = url.url.uriValue.toString();
+                        debugPrint('**** ----- !!!! URL del archivo: $fileUrl , MimeType: ${url.mimeType}');
+                        if (fileUrl.toLowerCase().startsWith('blob:') || url.mimeType == 'application/pdf' || fileUrl.toLowerCase().startsWith("data:application/octet-stream;base64,")){
+                          Fluttertoast.showToast(
+                            msg: 'BLOB, intentando descargar... \n ${url.mimeType} \n $fileUrl',
+                            backgroundColor: Colors.orange,
+                            textColor: Colors.white,
+                            fontSize: 16.0,
+                          );
+
+                          // Ejecuta JavaScript para convertir el Blob en un archivo descargable
+                          String jsCode = """
                           (async function() {
                             const blobUrl = "$fileUrl";
                             const response = await fetch(blobUrl);
@@ -162,29 +163,30 @@ class _MyAppState extends State<MyApp> {
                             }
                           })();
                         """;
-                        webViewController.evaluateJavascript(source: jsCode);
-                        return;
-                      } else {
-                         debugPrint('**** ----- else');
-                        Fluttertoast.showToast(
-                          msg: 'Url no reconocido \n $fileUrl',
-                          backgroundColor: Colors.red,
-                          textColor: Colors.white,
-                          fontSize: 16.0,
-                        );
-                      }
-                    },
-                  ),
-                ),
-                if (isDownloading)
-                  Container(
-                    color: Colors.black54,
-                    child: Center(
-                      child: CircularProgressIndicator(),
+                          webViewController.evaluateJavascript(source: jsCode);
+                          return;
+                        } else {
+                          debugPrint('**** ----- else');
+                          Fluttertoast.showToast(
+                            msg: 'Url no reconocido \n $fileUrl',
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0,
+                          );
+                        }
+                      },
                     ),
                   ),
-              ],
-            ),
+                ],
+              ),
+              if (isDownloading)
+                Container(
+                  color: Colors.black54,
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+            ]
           ),
         ),
         onPopInvokedWithResult: onPopInvokedWithResult,
